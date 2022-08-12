b5407ad82e2116f374a81a11174b8d858fc74617
swarajzz <sharma.swaraj0403@gmail.com>
2022-08-12 13:42:04 +0530

    Added ouline, box-shadow and isSelected

--- js/script.js
+++ js/script.js
@@ -6,15 +6,25 @@ const strongEl = document.querySelector("strong");
 const btnRatingEl = document.querySelectorAll(".btn--rating");
 const btnSubmitEl = document.querySelector(".btn--submit");
 
+let ratingSelected = false;
+btnSubmitEl.setAttribute("disabled", "");
 let rating = 5;
 let selectedBtn = 0;
 
+// When a rating is selected
 for (let i = 0; i < btnRatingEl.length; i++) {
   btnRatingEl[i].addEventListener("click", function () {
+    // If a rating is already selected, deselect it
     if (btnRatingEl[i].classList.contains("btn--selected")) {
+      ratingSelected = false;
+      btnSubmitEl.setAttribute("disabled", "");
       btnRatingEl[i].classList.remove("btn--selected");
       rating = 5;
+
+      // Select it
     } else {
+      ratingSelected = true;
+      btnSubmitEl.removeAttribute("disabled", "");
       btnRatingEl[selectedBtn].classList.remove("btn--selected");
       btnRatingEl[i].classList.add("btn--selected");
       selectedBtn = i;
@@ -24,7 +34,10 @@ for (let i = 0; i < btnRatingEl.length; i++) {
 }
 
 btnSubmitEl.addEventListener("click", function () {
-  strongEl.textContent = rating;
-  sectionFrontEl.classList.toggle("hidden");
-  sectionResultEl.classList.toggle("hidden");
+  if (ratingSelected) {
+    console.log("disabled inner");
+    strongEl.textContent = rating;
+    sectionFrontEl.classList.toggle("hidden");
+    sectionResultEl.classList.toggle("hidden");
+  }
 });
